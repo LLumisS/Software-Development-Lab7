3e2b1956d2732081c2ba8bdfbfa2261b6aea3ec1
LLumisS <vasa16671@gmail.com>
2023-12-12 18:39:43 +0200

    feat: flower tests

--- src/test/java/org/example/classes/flowers/FlowerTest.java
+++ src/test/java/org/example/classes/flowers/FlowerTest.java
@@ -1,5 +1,6 @@
 package org.example.classes.flowers;
 
+import org.example.classes.error.ExceptionHandler;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,21 +8,99 @@ import static org.junit.Assert.*;
 public class FlowerTest {
     @Test
     public void testConstructor() {
+        try {
+            // When
+            new Rose(-1, 10, 10);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Expected non-negative numbers", e.getMessage());
+        }
 
+        try {
+            // When
+            new Iris(10, -1, 10);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Expected non-negative numbers", e.getMessage());
+        }
+
+        try {
+            // When
+            new Tulip(10, 10, -1);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Expected non-negative numbers", e.getMessage());
+        }
+
+        // When
+        Rose rose = new Rose(8, 9, 10);
+        // Then
+        assertEquals(8, rose.getFreshness());
+        assertEquals(9, rose.getLength(), 0.01);
+        assertEquals(10, rose.getPrice(), 0.01);
     }
 
     @Test
     public void testWither() {
+        // When
+        Rose rose = new Rose(3, 10);
+        rose.wither();
+        rose.wither();
 
+        // Then
+        assertEquals(1, rose.getFreshness());
+
+        try {
+            // When
+            rose.wither();
+            rose.wither();
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("This flower is already withered...", e.getMessage());
+        }
     }
 
     @Test
     public void testCut() {
+        // When
+        Rose rose = new Rose(3, 10);
+        rose.cut(5);
 
+        // Then
+        assertEquals(5, rose.getLength(), 0.01);
+
+        try {
+            // When
+            rose.cut(10);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Cannot cut this much", e.getMessage());
+        }
+
+        try {
+            // When
+            rose.cut(-1);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Cannot cut negative length", e.getMessage());
+        }
     }
 
     @Test
     public void testSetPrice() {
+        // When
+        Rose rose = new Rose(1, 1);
+        rose.setPrice(15);
 
+        // Then
+        assertEquals(15, rose.getPrice(), 0.01);
+
+        try {
+            // When
+            rose.setPrice(-1);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Cannot set negative price", e.getMessage());
+        }
     }
 }
