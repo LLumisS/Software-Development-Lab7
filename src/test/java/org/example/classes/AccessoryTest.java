ca685d6c18af06cd6dd441555f972fa2bcef922d
LLumisS <vasa16671@gmail.com>
2023-12-12 18:47:44 +0200

    feat: accessory test

--- src/test/java/org/example/classes/AccessoryTest.java
+++ src/test/java/org/example/classes/AccessoryTest.java
@@ -1,5 +1,7 @@
 package org.example.classes;
 
+import org.example.classes.error.ExceptionHandler;
+import org.example.classes.flowers.Rose;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,11 +9,36 @@ import static org.junit.Assert.*;
 public class AccessoryTest {
     @Test
     public void testConstructor() {
+        try {
+            // When
+            new Accessory("ribbon", -1);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Cannot set negative price", e.getMessage());
+        }
 
+        // When
+        Accessory ribbon = new Accessory("ribbon", 10);
+        // Then
+        assertEquals("ribbon", ribbon.getName());
+        assertEquals(10, ribbon.getPrice(), 0.01);
     }
 
     @Test
     public void testSetPrice() {
+        // When
+        Accessory ribbon = new Accessory("ribbon", 10);
+        ribbon.setPrice(15);
 
+        // Then
+        assertEquals(15, ribbon.getPrice(), 0.01);
+
+        try {
+            // When
+            ribbon.setPrice(-1);
+        } catch (ExceptionHandler e) {
+            // Then
+            assertEquals("Cannot set negative price", e.getMessage());
+        }
     }
 }
